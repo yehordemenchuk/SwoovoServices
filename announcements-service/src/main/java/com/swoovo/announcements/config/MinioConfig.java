c7402e48294de857e473fa0bb75da7e2b4ff94a3
Yehor Demenchuk <demenchuk1210m@gmail.com>
2026-07-22 18:03:53 +0200

    add new services

--- announcements-service/src/main/java/com/swoovo/announcements/config/MinioConfig.java
+++ announcements-service/src/main/java/com/swoovo/announcements/config/MinioConfig.java
@@ -1,4 +1,4 @@
-package com.swoovo.announcements.dto.config;
+package com.swoovo.announcements.config;
 
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Value;
--- announcements-service/src/main/java/com/swoovo/announcements/config/RedisConfig.java
+++ announcements-service/src/main/java/com/swoovo/announcements/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.swoovo.announcements.dto.config;
+package com.swoovo.announcements.config;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
